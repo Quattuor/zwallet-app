c40eb37d58cef76b890d14858894cdc40bc42b13
Elwandy Tirtana Deriansyah <eltirder0@gmail.com>
2021-01-30 06:51:14 +0700

    comment persist gate

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {Provider} from 'react-redux';
-import {PersistGate} from 'redux-persist/integration/react';
+// import {PersistGate} from 'redux-persist/integration/react';
 
-import reduxStore from './utils/redux/store';
+import {store} from './utils/redux/store';
 import MainRouter from './router';
 
 const App = () => {
-  const {store, persistor} = reduxStore();
-
   return (
     <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <NavigationContainer>
-          <MainRouter />
-        </NavigationContainer>
-      </PersistGate>
+      {/* <PersistGate loading={null} persistor={persistor}> */}
+      <NavigationContainer>
+        <MainRouter />
+      </NavigationContainer>
+      {/* </PersistGate> */}
     </Provider>
   );
 };
