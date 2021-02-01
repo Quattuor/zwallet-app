e023da1675cc8d5b2061ac3b6474342e1927ac4f
Moch. Bayu Erich Dewangga <70324722+bayue48@users.noreply.github.com>
2021-02-01 16:07:46 +0700

    add history page

---
+++ src/assets/index.js
@@ -0,0 +1,14 @@
+//imgaes
+import bobi from './images/bobi.png';
+import momo from './images/momo.png';
+import netflix from './images/netflix.png';
+import spotify from './images/spotify.png';
+import susi from './images/susi.png';
+import topup from './images/topup.png';
+
+//icon
+import down from './icons/arrow-down.png';
+import left from './icons/arrow-left.png';
+import up from './icons/arrow-up.png';
+
+export {bobi, momo, netflix, spotify, susi, topup, down, left, up};
