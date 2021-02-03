0b47911aedd903554516918413192b5aab920b39
amardito <amardito37@gmail.com>
2021-02-04 05:44:10 +0700

    styling

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
 import {StyleSheet, Text, View, TouchableOpacity, Image} from 'react-native';
 import {connect} from 'react-redux';
